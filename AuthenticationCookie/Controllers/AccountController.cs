c8147578a1e2c5ea39a44c747e2c36bbcd7cd4d8
maxwitunski <max.witunski@onbase.com>
2017-04-10 15:21:01 -0400

    Added authentication cookie project.

---
+++ AuthenticationCookie/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthenticationCookie.Controllers
+{
+    [Route("account")]
+    public class AccountController : Controller
+    {
+        [HttpGet("signin")] // http://localhost:5000/account/signin
+        public async Task<IActionResult> SignIn()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "My Name", ClaimValueTypes.String),
+            };
+
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "Custom");
+            ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+            await HttpContext.Authentication.SignInAsync("OurAuthenticationCookie", principal);
+            var returnUrl = Request.Query["ReturnUrl"];
+            return Redirect($"../..{returnUrl}");
+        }
+    }
+}
---
+++ AuthenticationCookie/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AuthenticationCookie
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var cookieOptions = new CookieAuthenticationOptions()
+            {
+                AuthenticationScheme = "OurAuthenticationCookie",
+                LoginPath = new PathString("/account/signin"),
+                AccessDeniedPath = new PathString("/forbidden"),
+                AutomaticAuthenticate = false,
+                AutomaticChallenge = true,
+                CookieHttpOnly = true,
+            };
+
+            app.UseCookieAuthentication(cookieOptions);
+            app.UseMvc();
+
+        }
+    }
+}
